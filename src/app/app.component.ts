0d3a67435ba74873f761e027999b80263d33e869
peeoen <theerawat.pon@gmail.com>
2018-10-25 15:16:08 +0700

    update properties wysiwyg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wysiwyg-editor';
+  editorContent: string;
+  postImg: string;
+
+  options: any = {
+    dragInline: false,
+    tabSpaces: 4,
+    theme: 'gray',
+    fontFamilySelection: true,
+    fontSizeSelection: true,
+    paragraphFormatSelection: true,
+    charCounterCount: false,
+    fileInsertButtons: [],
+    imageUploadURL: 'http://localhost:50540/api/feeds/UploadImage',
+    fileUploadURL: 'http://localhost:50540/api/feeds/UploadFile',
+    toolbarButtons: [
+      'fontFamily', '|', 'fontSize', '|',
+      'color', '|',
+      'bold', 'italic', 'underline', 'subscript', 'superscript', '|',
+      'align', 'verticalAlign', '|',
+      'formatOL', 'formatUL', 'indent', 'outdent', '|',
+      'insertHR', 'specialCharacters', 'insertTable', 'insertImage', 'insertFile', '|',
+      'undo', 'redo', 'help'
+    ],
+    enableScript: false,
+    placeholderText: 'คุณกำลังคิดอะไรอยู่...',
+    events: {
+    },
+  };
+
+
 }
